34ab0dc6ce5d8971f21d8f5f6b7c55e8ff98e350
roceil <a0978006326@gmail.com>
2024-04-21 22:23:31 +0800

    feat(*): 測試 pre-commit

--- src/routes/healthyCheck.ts
+++ src/routes/healthyCheck.ts
@@ -1,7 +1,6 @@
 import { type RequestHandler, Router } from "express"
 import healthyCheckController from "../controllers/healthyCheckController"
 
-
 const router = Router()
 
 router.get("/healthy-check", healthyCheckController.getHealthyCheck as RequestHandler)
