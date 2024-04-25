c8680bb77d9aa62503b71cd14453b7f2a5f51ecf
roceil <a0978006326@gmail.com>
2024-04-25 14:51:21 +0800

    feat(*): 記錄 JWT 作法及測試 CORS

--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
 import express, { type Express } from "express"
-import connectDB from "./configs/dbConn"
 import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./configs/dbConn"
 
 import healthyCheckRouter from "@/routes/healthyCheck"
+import loginRouter from "@/routes/login"
+
 dotenv.config({ path: ".env.local" })
 
 const app: Express = express()
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 3001
+
+/* CORS */
+const corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 /* 解析 Body */
 app.use(express.json())
@@ -17,6 +29,7 @@ void connectDB()
 
 /* Router */
 app.use("/api", healthyCheckRouter)
+app.use("/api", loginRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
