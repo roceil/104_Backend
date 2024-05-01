4e43d85ed61732ad0e5d994cccf4c8500aa17679
roceil <a0978006326@gmail.com>
2024-05-01 20:54:22 +0800

    feat(*): include user models

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,92 +1,92 @@
-import { Schema, model, type Types } from "mongoose";
+import { Schema, model, type Types } from "mongoose"
 interface IPersonalInfo {
-  username: string;
-  email: string;
-  gender: string;
-  birthday: string;
+  username: string
+  email: string
+  gender: string
+  birthday: string
 }
 
 interface IUserSchema {
-  _id: Types.ObjectId;
-  personalInfo: IPersonalInfo;
-  preferences: object; // 資料代訂，不確定型別
-  isSubscribe: boolean;
-  points: number;
-  resetPasswordToken: string;
-  isActive: boolean;
-  blockedUsers: Types.ObjectId[];
-  notifications: Types.ObjectId[];
-  createdAt: Date;
-  updatedAt: Date;
+  _id: Types.ObjectId
+  personalInfo: IPersonalInfo
+  preferences: object // 資料代訂，不確定型別
+  isSubscribe: boolean
+  points: number
+  resetPasswordToken: string
+  isActive: boolean
+  blockedUsers: Types.ObjectId[]
+  notifications: Types.ObjectId[]
+  createdAt: Date
+  updatedAt: Date
 }
 
 const userSchema = new Schema<IUserSchema>(
   {
     _id: {
       type: Schema.Types.ObjectId,
-      required: [true, "userId is required"],
+      required: [true, "userId is required"]
     },
     personalInfo: {
       type: {
         username: {
           type: String,
-          required: [true, "username is required"],
+          required: [true, "username is required"]
         },
         email: {
           type: String,
-          required: [true, "email is required"],
+          required: [true, "email is required"]
         },
         gender: {
           type: String,
-          required: [true, "gender is required"],
+          required: [true, "gender is required"]
         },
         birthday: {
           type: String,
-          required: [true, "birthday is required"],
-        },
-      },
+          required: [true, "birthday is required"]
+        }
+      }
     },
     preferences: {},
     isSubscribe: {
       type: Boolean,
-      default: false,
+      default: false
     },
     points: {
       type: Number,
-      default: 0,
+      default: 0
     },
     resetPasswordToken: {
       type: String,
       default: "",
-      required: [true, "resetPasswordToken is required"],
+      required: [true, "resetPasswordToken is required"]
     },
     isActive: {
       type: Boolean,
       default: true,
-      required: [true, "isActive is required"],
+      required: [true, "isActive is required"]
     },
     blockedUsers: {
       type: [Schema.Types.ObjectId],
-      default: [],
+      default: []
     },
     notifications: {
       type: [Schema.Types.ObjectId],
-      default: [],
+      default: []
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      required: [true, "createdAt is required"],
+      required: [true, "createdAt is required"]
     },
     updatedAt: {
       type: Date,
       default: Date.now,
-      required: [true, "updatedAt is required"],
-    },
+      required: [true, "updatedAt is required"]
+    }
   },
   { versionKey: false }
-);
+)
 
-const User = model<IUserSchema>("user", userSchema);
+const User = model<IUserSchema>("user", userSchema)
 
-export default User;
+export default User
