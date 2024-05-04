dceb06f7c22e00a5920450e3f0b0f85d8f9d6703
CHUN-CHIA <a121515222@gmail.com>
2024-05-04 21:48:48 +0800

    fix 調整handleErrorSaync內的catch

--- src/middlewares/asyncErrorHandler.ts
+++ src/middlewares/asyncErrorHandler.ts
@@ -1,14 +1,18 @@
 import { type Request, type Response, type NextFunction } from "express"
 
 // 定義一個類型，表示接受 Express 的 req, res, next 並返回 Promise 的異步函數
-type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<unknown>
+type AsyncFunction = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<unknown>
 
-const handleErrorAsync = (func: AsyncFunction) => {
+const handleErrorAsync = (func: AsyncFunction): unknown => {
   return function (req: Request, res: Response, next: NextFunction) {
-    console.log("asyncErrorHandler")
-
     // NOTE：這裡的 next 不知原因無法被 app 的 mongoDB.errorHandler 捕捉到
-    func(req, res, next).catch(next)
+    func(req, res, next).catch((err: Error) => {
+      next(err)
+    })
   }
 }
 
