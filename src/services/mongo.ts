6c8e018c4c55e61d5844a5e7bd7ccc55669f42cd
roceil <a0978006326@gmail.com>
2024-05-04 23:35:34 +0800

    feat(errordev/prod): 拆分正式/測試環境錯誤捕捉函式

--- src/services/mongo.ts
+++ src/services/mongo.ts
@@ -1,35 +1,7 @@
 import { type Request, type Response, type NextFunction } from "express"
-
-interface CustomError extends Error {
-  statusCode?: number
-  isOperational?: boolean
-}
-
-// 開發環境
-const resErrorDev = (err: CustomError, res: Response) => {
-  const statusCode = err.statusCode ?? 500 // 預設值為 500
-  res.status(statusCode).json({
-    message: err.message,
-    error: err,
-    stack: err.stack
-  })
-}
-
-// 正式環境
-const resErrorProd = (err: CustomError, res: Response) => {
-  const statusCode = err.statusCode ?? 500 // 預設值為 500
-  if (err.isOperational) { // 如果可操作的錯誤
-    res.status(statusCode).json({
-      message: err.message
-    })
-  } else {
-    console.error("[server]：出現系統錯誤", err)
-    res.status(500).json({
-      status: "error",
-      message: "系統錯誤，請稍後再試！"
-    })
-  }
-}
+import { type CustomError } from "@/types/errors"
+import resErrorDev from "@/utils/resErrorDev"
+import resErrorProd from "@/utils/resErrorProd"
 
 const errorHandler = (err: CustomError, req: Request, res: Response, _next: NextFunction) => {
   const statusCode = err.statusCode ?? 500
