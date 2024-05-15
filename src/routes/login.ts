5e1e76f687f7cfe17f9a1659c094968df490b1c8
a121515222 <a121515222@hotmail.com>
2024-05-15 17:11:23 +0800

    feat:完成註冊、登入、忘記密碼、修改密碼swagger文件

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -52,24 +52,91 @@ router.post("/sign-up", asyncErrorHandler(
 router.post("/login", asyncErrorHandler(
   /**
    * #swagger.tags = ["LogInAndSignUp-登入與註冊"]
+   * #swagger.parameters['body'] = {
+      in: "body",
+      required: true,
+      type: "Object",
+      description: "登入資訊",
+      schema: {
+        $account: "56asdf@hotmail.com",
+        $password: "a1234567",
+        }
+      }
+    * #swagger.responses[200] = {
+      description: '登入成功',
+      schema: {
+        status: true,
+        message: "登入成功",
+        data: {
+            userId: "66441880635c6a9bc95c164b",
+            email:"56asdf@hotmail.com",
+            name: "Eason",
+            gender: null,
+            birthday: null,
+            token: "as4d5fa421sdfasdf",
+            }
+        }
+      }
    */
   loginController.login) as RequestHandler)
 
 router.put("/reset-password", isAuth as Handler, asyncErrorHandler(
   /**
    * #swagger.tags = ["LogInAndSignUp-登入與註冊"]
+   * #swagger.security = [{
+      "apiKeyAuth":[]
+    }]
+  * #swagger.parameters['body'] = {
+      in: "body",
+      required: true,
+      type: "Object",
+      description: "修改密碼",
+      schema: {
+        $newPassword: "a1234567",
+        $confirmNewPassword: "a1234567"
+      }
+    }
+  * #swagger.responses[200] = {
+    description: '修改成功',
+    schema: {
+      status: true,
+      message: "修改密碼成功",
+      data: null
+      }
+    }
    */
   loginController.resetPassword) as RequestHandler)
 
 router.post("/forget-password", asyncErrorHandler(
   /**
    * #swagger.tags = ["LogInAndSignUp-登入與註冊"]
+   * #swagger.parameters['body'] = {
+      in: "body",
+      required: true,
+      type: "Object",
+      description: "忘記密碼",
+      schema: {
+        $email: "asdfa@hotmail.com"
+      }
+    }
+   * #swagger.responses[200] = {
+      description: '郵件寄送成功',
+      schema: {
+        status: true,
+        message: "郵件寄送成功",
+        data: null
+      }
+    }
    */
   loginController.forgetPassword) as RequestHandler)
 
 router.get("/verify", isAuth as Handler, asyncErrorHandler(
   /**
    * #swagger.tags = ["LogInAndSignUp-登入與註冊"]
+   * #swagger.description = "把拿到的Token拿到Authorize中，進行驗證。"
+   * #swagger.security = [{
+      "apiKeyAuth":[]
+    }]
    */
   loginController.verifyToken) as RequestHandler)
 
