ba0c0d8061cf6dd53f40efc5569264e75720fb68
eric <toto9999912@gmail.com>
2024-06-19 20:58:45 +0800

    feat:新增socketi o聊天室 新增聊天室schema

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,9 +1,29 @@
+// src/middlewares/auth.ts
 import { type Request, type Response, type NextFunction } from "express"
 import jwt from "jsonwebtoken"
 import appErrorHandler from "@/utils/appErrorHandler"
 
+export const verifyToken = (token: string, next: NextFunction) => {
+  const key = process.env.JWT_SECRET
+  if (!key) {
+    appErrorHandler(500, "缺少必要環境變數", next)
+    return null
+  }
+
+  try {
+    const decoded = jwt.verify(token, key)
+    return decoded
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      appErrorHandler(401, "token 已過期", next)
+    } else {
+      appErrorHandler(401, "驗證失敗", next)
+    }
+    return null
+  }
+}
+
 const isAuth = (req: Request, res: Response, next: NextFunction) => {
-  // 檢查 cookie 中是否有 token
   const token = req.cookies.token as string || req.params.token
 
   if (!token) {
@@ -11,38 +31,12 @@ const isAuth = (req: Request, res: Response, next: NextFunction) => {
     return
   }
 
-  // 提取 JWT 必要環境變數
-  const key = process.env.JWT_SECRET
-  if (!key) {
-    appErrorHandler(500, "缺少必要環境變數", next)
-    return
-  }
-
-  // 移除 Bearer 字串並確保 tokenString 不為 undefined
-  // const tokenParts = token
-  // const tokenString = tokenParts
-  // if (!tokenString) {
-  //   appErrorHandler(401, "無效的 token 格式", next)
-  //   return
-  // }
-
-  // 驗證 token 並將解碼後的資料存入 req.user
-  jwt.verify(token, key, (err, decoded) => {
-    if (err) {
-      if (err instanceof jwt.TokenExpiredError) {
-        appErrorHandler(401, "token 已過期", next)
-        return
-      } else {
-        appErrorHandler(401, "驗證失敗", next)
-        return
-      }
-    }
-
+  const decoded = verifyToken(token, next)
+  if (decoded) {
     req.user = decoded
     req.token = token
-  })
-
-  next()
+    next()
+  }
 }
 
 export default isAuth
