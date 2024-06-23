283c4da6facd04f26089dbff501db832516a7ff7
roceil <a0978006326@gmail.com>
2024-06-23 19:26:49 +0800

    feat(neweb-pay): 增加log 訊息
    feat(*): test newebpay
    
    feat(*): test
    
    feat(*): test
    
    feat(*): test
    
    feat(*): test

--- src/routes/ordersRouter.ts
+++ src/routes/ordersRouter.ts
@@ -1,10 +1,14 @@
 import { type RequestHandler, Router, type Handler } from "express"
-import { createOrder } from "@/services/newebPay"
+import { createOrder, notifyOrder, returnOrder } from "@/services/newebPay"
 import asyncErrorHandler from "@/middlewares/asyncErrorHandler"
 import isAuth from "@/middlewares/isAuth"
 
 const router = Router()
 
 router.post("/create-order", isAuth as Handler, asyncErrorHandler(createOrder) as RequestHandler)
+
+router.post("/notify", asyncErrorHandler(notifyOrder) as RequestHandler)
+
+router.post("/return", asyncErrorHandler(returnOrder) as RequestHandler)
 
 export default router
