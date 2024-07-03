2d6c37c300023271b28808df14562778d8090445
roceil <a0978006326@gmail.com>
2024-07-03 10:17:25 +0800

    feat(happiness-example): 完成幸福案例 CR API

--- src/controllers/happinessExampleController.ts
+++ src/controllers/happinessExampleController.ts
@@ -1,9 +1,51 @@
 import { type NextFunction, type Request, type Response } from "express"
+import { v4 as uuidv4 } from "uuid"
+import * as admin from "firebase-admin"
+import { getStorage } from "firebase-admin/storage"
+import { config, compressImage } from "@/services/firebase"
 import { type LoginResData } from "@/types/login"
 import HappinessExample from "@/models/happinessExample"
 import appSuccessHandler from "@/utils/appSuccessHandler"
 import appErrorHandler from "@/utils/appErrorHandler"
 import checkMissingFields from "@/utils/checkMissingFields"
+
+// 初始化 Firebase Admin SDK
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(config as admin.ServiceAccount),
+    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`
+  })
+}
+
+/**
+ * 上傳圖片到 Firebase
+ */
+const uploadToFirebase = async (buffer: Buffer, fileName: string): Promise<string> => {
+  return await new Promise((resolve, reject) => {
+    const bucket = getStorage().bucket()
+    const blob = bucket.file(`happinessExample/${fileName}`)
+    const blobStream = blob.createWriteStream()
+
+    blobStream.on("finish", () => {
+      blob.getSignedUrl({
+        action: "read",
+        expires: "12-31-2500"
+      }, (err, url) => {
+        if (err) {
+          reject(err || new Error("獲取簽名網址失敗"))
+        } else {
+          resolve(url as unknown as string)
+        }
+      })
+    })
+
+    blobStream.on("error", (err) => {
+      reject(err)
+    })
+
+    blobStream.end(buffer)
+  })
+}
 
 /**
  * 取得所有幸福案例
@@ -42,7 +84,90 @@ export const getExampleById = async (req: Request, res: Response, next: NextFunc
     return
   }
 
-  appSuccessHandler(200, "成功取得幸福案例", example, res)
+  // 隨機取得其他幸福案例
+  const otherExamples = await HappinessExample.aggregate([
+    { $match: { _id: { $ne: id } } },
+    { $sample: { size: 3 } }
+  ])
+
+  const resData = {
+    mainExample: example,
+    relatedExample: otherExamples
+  }
+
+  appSuccessHandler(200, "成功取得幸福案例", resData, res)
+}
+
+/**
+ * 上傳幸福案例封面圖片
+ */
+export const uploadExampleCoverImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  // const { userId } = req.user as LoginResData
+
+  const { file } = req
+
+  if (!file) {
+    appErrorHandler(400, "請上傳圖片", next)
+    return
+  }
+
+  // 壓縮圖片
+  const compressedImage = await compressImage(file.buffer)
+
+  // 生成唯一檔案名稱
+  const fileName = `${uuidv4()}.${file.originalname.split(".").pop()}`
+
+  // 上傳圖片到 Firebase
+  const url = await uploadToFirebase(compressedImage, fileName)
+
+  if (!url) {
+    appErrorHandler(500, "上傳失敗，請稍後再試", next)
+    return
+  }
+
+  const resData = {
+    coverImage: url
+  }
+
+  appSuccessHandler(200, "上傳成功", resData, res)
+}
+
+/**
+ * 上傳幸福案例封面圖片
+ */
+export const uploadExampleImages = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { files } = req
+
+  if (!files || !Array.isArray(files) || files.length === 0) {
+    appErrorHandler(400, "請上傳圖片", next)
+    return
+  }
+
+  const uploadPromises = files.map(async (file: Express.Multer.File) => {
+    // 壓縮圖片
+    const compressedImage = await compressImage(file.buffer)
+
+    // 生成唯一檔案名稱
+    const fileName = `${uuidv4()}.${file.originalname.split(".").pop()}`
+
+    // 上傳圖片到 Firebase
+    const url = await uploadToFirebase(compressedImage, fileName)
+
+    if (!url) {
+      appErrorHandler(500, "上傳失敗，請稍後再試", next)
+      return
+    }
+
+    return url
+  })
+
+  const urls = await Promise.all(uploadPromises)
+
+  const resData = {
+    images: urls
+  }
+
+  appSuccessHandler(200, "上傳成功", resData, res)
 }
 
 /**
@@ -51,7 +176,7 @@ export const getExampleById = async (req: Request, res: Response, next: NextFunc
 export const addExample = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { userId } = req.user as LoginResData
 
-  const { myName, partnerName, coverImage, title, content } = req.body
+  const { myName, partnerName, coverImage, imgList = [], title, content } = req.body
 
   const missingFields = checkMissingFields({ myName, partnerName, coverImage, title, content })
 
@@ -66,6 +191,7 @@ export const addExample = async (req: Request, res: Response, next: NextFunction
     myName,
     partnerName,
     coverImage,
+    imgList,
     title,
     content
   })
--- src/routes/happinessExampleRouter.ts
+++ src/routes/happinessExampleRouter.ts
@@ -1,16 +1,19 @@
 import { type RequestHandler, Router, type Handler } from "express"
-import { getAllExamples, getExampleById, addExample } from "@/controllers/happinessExampleController"
+import { getAllExamples, getExampleById, addExample, uploadExampleCoverImage, uploadExampleImages } from "@/controllers/happinessExampleController"
 import asyncErrorHandler from "@/middlewares/asyncErrorHandler"
 import isAuth from "@/middlewares/isAuth"
-// import multer from "multer"
+import { uploadSingleFile, uploadMultipleFiles } from "@/middlewares/uploadImage"
 
 const router = Router()
-// const upload = multer()
 
 router.get("/happiness-example", asyncErrorHandler(getAllExamples) as RequestHandler)
 
 router.get("/happiness-example/:id", asyncErrorHandler(getExampleById) as RequestHandler)
 
 router.post("/happiness-example", isAuth as Handler, asyncErrorHandler(addExample) as RequestHandler)
+
+router.post("/happiness-cover-image", isAuth as Handler, uploadSingleFile, asyncErrorHandler(uploadExampleCoverImage) as RequestHandler)
+
+router.post("/happiness-multiple-image", isAuth as Handler, uploadMultipleFiles, asyncErrorHandler(uploadExampleImages) as RequestHandler)
 
 export default router
