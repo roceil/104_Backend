1eecb2b251ea4a4c2d0682f0467b565d3d4c92d7
roceil <a0978006326@gmail.com>
2024-07-03 16:57:54 +0800

    docs(*): 增加幸福案例 swagger

--- src/routes/happinessExampleRouter.ts
+++ src/routes/happinessExampleRouter.ts
@@ -3,17 +3,18 @@ import { getAllExamples, getExampleById, addExample, uploadExampleCoverImage, up
 import asyncErrorHandler from "@/middlewares/asyncErrorHandler"
 import isAuth from "@/middlewares/isAuth"
 import { uploadSingleFile, uploadMultipleFiles } from "@/middlewares/uploadImage"
+import { getAllExamplesSwagger, getExampleByIdSwagger, addExampleSwagger, uploadExampleCoverImageSwagger, uploadExampleImagesSwagger } from "@/middlewares/swaggerConfig/happinessExampleSwagger"
 
 const router = Router()
 
-router.get("/happiness-example", asyncErrorHandler(getAllExamples) as RequestHandler)
+router.get("/happiness-example", getAllExamplesSwagger, asyncErrorHandler(getAllExamples) as RequestHandler)
 
-router.get("/happiness-example/:id", asyncErrorHandler(getExampleById) as RequestHandler)
+router.get("/happiness-example/:id", getExampleByIdSwagger, asyncErrorHandler(getExampleById) as RequestHandler)
 
-router.post("/happiness-example", isAuth as Handler, asyncErrorHandler(addExample) as RequestHandler)
+router.post("/happiness-example", addExampleSwagger, isAuth as Handler, asyncErrorHandler(addExample) as RequestHandler)
 
-router.post("/happiness-cover-image", isAuth as Handler, uploadSingleFile, asyncErrorHandler(uploadExampleCoverImage) as RequestHandler)
+router.post("/happiness-cover-image", uploadExampleCoverImageSwagger, isAuth as Handler, uploadSingleFile, asyncErrorHandler(uploadExampleCoverImage) as RequestHandler)
 
-router.post("/happiness-multiple-image", isAuth as Handler, uploadMultipleFiles, asyncErrorHandler(uploadExampleImages) as RequestHandler)
+router.post("/happiness-multiple-image", uploadExampleImagesSwagger, isAuth as Handler, uploadMultipleFiles, asyncErrorHandler(uploadExampleImages) as RequestHandler)
 
 export default router
